7252a3e91cba82371d9c96809a80f2537ac271da
Carine Batista <carinedcb@hotmail.com>
2021-05-10 17:59:58 -0300

    Add Hook useForm and Api Fetch

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,33 +2,52 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Input from '../Forms/Input'
 import Button from '../Forms/Button'
+import useForm from '../../Hooks/useForm'
+import { TOKEN_POST, USER_GET } from '../../api'
 
  const LoginForm = () => {
-	const [username, setUsername] = React.useState('')
-	const [password, setPassword] = React.useState('')
 
-	function handleSubmit(event){
+	const username = useForm()
+	const password = useForm()
+
+
+	React.useEffect(() => {
+		const token = window.localStorage.getItem('token')
+		if(token) {
+			getUser(token)
+		}
+
+	}, [])
+
+	async function getUser(token) {
+		const {url, options} = USER_GET(token)
+		const response = await fetch(url, options)
+		const json = await response.json()
+		console.log(json)
+	}
+
+	async function handleSubmit(event){
 		event.preventDefault()
-		fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-			method: 'POST', 
-			header: {
-				'Content-Type': 'application/json',
-			},
-			body:JSON.stringify({username, password}),
-		}).then((response) =>{
-			console.log(response)
-			return response.json()
-		}).then((json) => {
-			console.log(json)
-		})
+
+		if(username.validate() && password.validate()) {
+			const {url, options} = TOKEN_POST({
+				username: username.value, 
+				password: password.value,
+			})
+
+			const response = await fetch(url, options)
+			const json = await response.json()
+			window.localStorage.setItem('token', json.token)
+			getUser(json.token)
+		}
 	}
 
 	return (
 		<section>
 			<h1> Login </h1>
 			<form action = "" onSubmit={handleSubmit}>
-				<Input label="Usuario" type="text" name="username"/>
-				<Input label="Senha" type="password" name="password"/>
+				<Input label="Usuario" type="text" name="username" {...username} />
+				<Input label="Senha" type="password" name="password" {...password} />
 
 			<Button> Entrar </Button>
 

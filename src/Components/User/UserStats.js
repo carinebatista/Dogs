de7cad94a1e6feeefa87bbb9d46b6dac29239565
Carine Batista <carinedcb@hotmail.com>
2021-05-14 12:32:16 -0300

    Final Adjusts

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,34 +1,32 @@
-import React from 'react'
-import Head from '../Helper/Head'
-import useFetch from '../../Hooks/useFetch'
-import { STATS_GET } from '../../Api'
-import Loading from '../Helper/Loading'
-import Error from '../Helper/Error'
-import UserStatsGraph from './UserStatsGraph'
+import React from 'react';
+import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../Api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+const UserStatsGraph = React.lazy(() => import('./UserStatsGraph'));
 
 const UserStats = () => {
+  const { data, error, loading, request } = useFetch();
 
-	const {data, error, loading, request} = useFetch()
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
 
-	React.useEffect(() => {
-		async function getData(){
-			const {url, options} = STATS_GET()
-			await request(url, options)
-		}
-		getData()
-	}, [request])
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraph data={data} />
+      </React.Suspense>
+    );
+  else return null;
+};
 
-	if(loading) return <Loading />
-	if(error) return <Error error={error} />
-	if(data) 
-		return (
-			<div>
-			<Head title="Estastísticas"/> 
-
-				<UserStatsGraph data={data}/>
-			</div>
-		)
-	else return null
-}
-
-export default UserStats
+export default UserStats;
